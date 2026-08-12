69680327199860de48cf437ae08ae9054daafc9a
Tanvi Shetty <shettytanvi19@gmail.com>
2026-08-12 15:00:25 +0530

    feat(api): register POST /books route in main router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,6 +21,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /books", h.GetAllBooks)
 	mux.HandleFunc("GET /books/{id}", h.GetBookByID)
+	mux.HandleFunc("POST /books", h.CreateBook)
 	//Start Server
 	fmt.Println("Bookstore API server running on http://localhost:8080...")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
